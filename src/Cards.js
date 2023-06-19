1a7240de3dc3cce6f5d8829432b4b08a35b1e0e3
Harry Steele <harrybsteele@gmail.com>
2023-06-18 22:26:29 -0400

    designed cards

--- src/Cards.js
+++ src/Cards.js
@@ -2,23 +2,29 @@ import React from "react";
 import { Data } from "./Data";
 import "./Cards.css";
 import chip from "./chip.png";
+import { useState } from "react";
 
 const Cards = () => {
   const currentCards = Data[Data.currentUser].cards;
-  console.log(currentCards);
+
+  const [active, setActive] = useState(0);
+  const activeCard = currentCards[active];
+  console.log(activeCard)
   return (
     <>
 
       <div id="cards-container">
         <h2 id="cards-container-title">My Cards</h2>
 
-        {currentCards.map((card) => {
+        {currentCards.map((card, i) => {
           return (
-            <div className="card">
+            <div className={activeCard.cardNumber === card.cardNumber ? "active-card" : "card"} onClick={() => setActive(i)}>
               <img id="card-chip-img" src={chip} />
               <p id="balance">Balance</p>
               <p id="balance-total">${card.balance}</p>
               <p id="card-type">{card.cardType}</p>
+              <p id="expiration">Good Thru</p>
+              <p id="card-date">{card.month}/{card.year}</p>
             </div>
           );
         })}
