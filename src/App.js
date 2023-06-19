d4f7b0cca18566b30ba670274911d9d3b402b4c0
Harry Steele <harrybsteele@gmail.com>
2023-06-19 12:51:22 -0400

    updated card styles

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import logo from './logo.svg';
 import './App.css';
-import Cards from "./Cards"
+import Dashboard from './Dashboard';
 
 function App() {
   return (
-      <Cards />
+      <Dashboard />
   );
 }
 
---
+++ src/Dashboard.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Cards from "./Cards";
+
+const Dashboard = () => {
+
+    return (
+        <>
+        <Cards />
+        </>
+    )
+
+}
+
+export default Dashboard
