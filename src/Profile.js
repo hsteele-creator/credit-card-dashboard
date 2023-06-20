54ce3ea0a2cb794d90c6466bf2ff10900f779168
Harry Steele <harrybsteele@gmail.com>
2023-06-20 09:37:11 -0400

    working on avatar section

--- src/Profile.js
+++ src/Profile.js
@@ -3,19 +3,28 @@ import "./Profile.css";
 import { useSelector } from "react-redux";
 
 const Profile = () => {
-    const store = useSelector(state => state.Data);
-    const currentCards = store[store.currentUser].cards;
-    const balance = currentCards.map(card => card.balance).map(string => Number(string)).reduce((acc, next) => acc + next)
-    console.log(balance);
+  const store = useSelector((state) => state.Data);
+  const currentCards = store[store.currentUser].cards;
+  const balance = currentCards
+    .map((card) => card.balance)
+    .map((string) => Number(string))
+    .reduce((acc, next) => acc + next);
+  const currentUserImage = store[store.currentUser].image;
+  const currentUserName = store[store.currentUser].name.split(" ")[0];
+  console.log(currentUserName);
+
   return (
     <>
       <div id="profile-main">
         <div id="balance-container">
-            <p id="balance-heading">Wallet balance:</p>
-            <p id="total">${balance}.00</p>
+          <p id="balance-heading">Wallet balance:</p>
+          <p id="total">${balance}.00</p>
         </div>
 
-        <div id="avatar-container"></div>
+        <div id="current-avatar-container">
+            <p>Hello {currentUserName}</p>
+            <img id="current-user-image" src={currentUserImage} />
+        </div>
       </div>
     </>
   );
