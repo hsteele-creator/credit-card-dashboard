cfdd88237fd9c98794d6d89ae1e0f262dbaefb51
Harry Steele <harrybsteele@gmail.com>
2023-06-22 14:42:25 -0400

    added search functionality to transactions

--- src/Transactions.js
+++ src/Transactions.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Data } from "./Data";
 import { useDispatch, useSelector } from "react-redux";
+import { useState } from "react";
 import Transaction from "./Transaction";
 import "./Transactions.css";
 import Profile from "./Profile";
@@ -12,11 +13,45 @@ const Transactions = () => {
   const currentCard = currentCards[store.currentCard];
   const currentTransactions = currentCard.transactions;
 
+  // const [transactions, setTransactions] = useState(currentTransactions);
+  const [search, setSearch] = useState("");
+
+
+
+  const filteredTransactions = () => {
+    if(search === "") {
+      return currentTransactions
+    } else {
+      return currentTransactions.filter(transaction => transaction.title.includes(search))
+    }
+  }
+
+  const handleSearch = (e) => {
+    setSearch(search => e.target.value);
+
+    return filtered
+
+  }
+
+  const filtered = filteredTransactions();
+
   return (
     <>
       <div id="transactions-main">
-        <h2 id="transaction-title">Card <span>History</span></h2>
-        {currentTransactions.map((transaction) => {
+        <h2 id="transaction-title">
+          Card <span>History</span>
+        </h2>
+
+        <div id="search-form">
+          <input
+            placeholder="search by title"
+            onChange={(e) => handleSearch(e)}
+            value={search}
+          ></input>
+          <button id="search-button" onClick={() => setSearch("")}>Reset</button>
+        </div>
+
+        {filtered.map((transaction) => {
           return (
             <Transaction
               title={transaction.title}
